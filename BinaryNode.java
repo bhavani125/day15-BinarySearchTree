6402f7413a0f1211b5cd9cfb0dbb3fa79789599a
bhavani125 <girinenibhavani12@gmil.com>
2021-07-21 23:34:03 +0530

    Added: Code of creating binarySearchTree

---
+++ BinaryNode.java
@@ -0,0 +1,19 @@
+package com.tree;
+
+public class BinaryNode<K extends Comparable<K>>  {
+    K key;
+    BinaryNode<K> left;
+    BinaryNode<K> right;
+
+    public BinaryNode(K key)
+    {
+        super();
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+    @Override
+    public String toString() {
+        return "BinaryNode{" + "key=" + key + ", left=" + left + ", right=" + right + '}';
+    }
+}
--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,7 +1,40 @@
 package com.tree;
 
-public class BinarySearchTree {
+public class BinarySearchTree<K extends Comparable<K>> {
     public static void main(String[] args) {
         System.out.println("Welcome to BinarySearchTree program");
+        BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        System.out.println(binaryTree);
+    }
+    private BinaryNode<K> root;
+    public void add(K key)
+    {
+        this.root = this.insertData(root,key);
+    }
+
+    public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+    {
+        if(current ==  null)
+            return new BinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if(compareResult==0)
+            return current;
+        if(compareResult<0)
+        {
+            current.left = insertData(current.left, key);
+        }
+        else
+        {
+            current.right = insertData(current.right, key);
+        }
+        return current;
+    }
+    @Override
+    public String toString()
+    {
+        return "MyBinary [root=" + root + "]";
     }
 }
